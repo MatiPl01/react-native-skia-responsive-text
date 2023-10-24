28ccbee50f58b9cdb5b32872d36c7e1ba8ef4daf
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2023-10-24 23:36:02 +0200

    Add example app setup

--- example/metro.config.js
+++
@@ -1,8 +0,0 @@
-const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
-const path = require("path");
-
-const config = {
-  watchFolders: [path.resolve(__dirname, "../node_modules")],
-};
-
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
